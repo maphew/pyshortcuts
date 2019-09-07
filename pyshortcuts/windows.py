956fa097ae35bcf2faffcdc8d681bc3c62d66546
matt wilkie <maphew@gmail.com>
2019-09-06 22:13:06 -0700

    use Eryk Sun's knownfolders via ctypes
    more correct, but also heavyweight, since in the shortcut context we
    don't need anything other than Menu and Desktop, and the old int ID
    works just fine for that.

--- pyshortcuts/windows.py
+++ pyshortcuts/windows.py
@@ -10,24 +10,25 @@ from .utils import get_homedir
 from .shortcut import Shortcut
 
 import win32com.client
-shellapp = win32com.client.Dispatch("Shell.Application")
 objshell = win32com.client.Dispatch("Wscript.Shell")
-
 
 def get_exe_types():
     '''Return list of valid executable file extensions [.com, .exe, ...]'''
     exetypes = [ext.lower() for ext in os.environ['PATHEXT'].split(os.pathsep)]
     return exetypes
 
-# Windows Special Folders
-# ID numbers from https://gist.github.com/maphew/47e67b6a99e240f01aced8b6b5678eeb
-# https://docs.microsoft.com/en-gb/windows/win32/api/shldisp/ne-shldisp-shellspecialfolderconstants#constants
+
+# Windows Known Folders (desktop, startmenu, my documents, ...)
+import winknownfolders as kf
+kf.getpath = kf.get_known_folder_path
+
 def get_menu_folder():
-    '''Return user Start Menu folder'''
-    return shellapp.namespace(11).self.path
+    '''Return user Start Menu folder path'''
+    return kf.getpath(kf.FOLDERID.StartMenu)
+
 def get_desktop_folder():
-    '''Return user Desktop folder'''
-    return shellapp.namespace(0).self.path
+    '''Return user Desktop folder path'''
+    return kf.getpath(kf.FOLDERID.Desktop)
 
 
 def make_shortcut(script, name=None, description=None, terminal=True,
@@ -40,7 +41,7 @@ def make_shortcut(script, name=None, description=None, terminal=True,
     name        (str or None) name to use for shortcut [defaults to script name]
     description (str or None) longer description of script [defaults to `name`]
     icon        (str or None) path to icon file [defaults to python icon]
-    folder      (str or None) folder on Desktop to put shortcut [defaults to Desktop]
+    folder      (str or None) where to put shortcut [defaults to Desktop]
     terminal    (True or False) whether to run in a Terminal  [True]
     """
 
